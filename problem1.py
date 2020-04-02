2e3ca6e577199114ebd621e61ac162716c0eac27
squeaky1273 <pjriddell22@gmail.com>
2020-04-01 22:09:39 -0700

    practiced techincal problems

--- problem1.py
+++ problem1.py
@@ -1,9 +1,32 @@
 # Question:
+    # Given an array of integers, 
+    # return indices of the two numbers such that 
+    # they add up to a specific target.
 
 # Restate:
+    # Return the index of 2 numbers that 
+    # add up to a target determined later
 
 # Clarifying Qs:
+    # Can the same number be used twice?
 
 # Assumptions:
+    # Each input has one solution
+    # can't be used twice
 
-# Thoughts:
+# Thoughts/Solution:
+    # need some sort of loop to go throught the array
+
+def sum_of_two(arr, target):
+    for num1 in range(len(arr)): # For as long as num1 is in the array
+        for num2 in range(num1+1,len(arr)): # For as long as num2 is in the array
+            if arr[num1] + arr[num2] == target: # If num1 and num2 in the array equal the target,
+                return (num1, num2) # return the index where num1 and num2 are at in the array.
+    return None # if there aren't numbers in the array that equal the target, return None 
+ 
+# Create a sample array
+numbers = [1, 3, 5, 7, 10]
+print(sum_of_two(numbers, 9)) # Should return None
+print(sum_of_two(numbers, 8)) # Should return (0, 3)
+
+
--- problem2.py
+++ problem2.py
@@ -1,9 +1,28 @@
 # Question:
+    # Given an array of integers, find if the array contains any duplicates.
 
 # Restate:
+    # Indicate if a given arrray have duplicate integers
 
 # Clarifying Qs:
+    # How should I indicate whethter duplicates exist?
 
 # Assumptions:
+    # If there are duplicates, return True
+    # If there aren't duplicates, return False
 
-# Thoughts:
+# Thoughts/Solution:
+
+def find_if_duplicate(arr):
+    for num1 in range(len(arr)): # For as long as num1 is in the array
+        for num2 in range(0, num1): # For as long as num2 is in the array
+            if arr[num1] == arr[num2]: # If 2 integers are equal to each other, 
+                return True # Return True (there is a duplicate)
+    return False # else return False (no duplcates)
+
+# Create sample array
+arr = [1, 2, 3, 4, 5]
+print(find_if_duplicate(arr)) # Should return False (no duplicates)
+
+arr = [1, 2, 3, 4, 4, 5]
+print(find_if_duplicate(arr)) # Should return True (duplicates exist)
